27950dc9cad01a31ccd41c6acd6aaa27f9b014a6
alberet16 <105561070+alberet16@users.noreply.github.com>
2023-09-05 14:48:55 +0700

    pilot_home

--- lib/app/modules/efb/pilot/controllers/homepilot_controller.dart
+++ lib/app/modules/efb/pilot/controllers/homepilot_controller.dart
@@ -1,11 +1,47 @@
 import 'package:get/get.dart';
 
+import '../../../../../data/users/user_preferences.dart';
+import '../../../../../di/locator.dart';
+
 class HomePilotController extends GetxController {
   //TODO: Implement HomeccController
+  late UserPreferences userPreferences;
+  late String titleToGreet;
+  late String timeToGreet;
 
   final count = 0.obs;
   @override
   void onInit() {
+    userPreferences = getItLocator<UserPreferences>();
+
+
+    switch (userPreferences.getRank()) {
+      case 'CAPT':
+        titleToGreet = 'Captain';
+        break;
+      case 'FO':
+        titleToGreet = 'First Officer';
+        break;
+      case 'Pilot Administrator':
+        titleToGreet = 'Pilot Administrator';
+        break;
+      default:
+        titleToGreet = 'Allstar';
+    }
+
+    // assessmentResults = [];
+    // assessmentResultsNotConfirmedByCPTS = [];
+
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      timeToGreet = "Morning";
+    } else if (hour < 17) {
+      timeToGreet = "Afternoon";
+    } else {
+      timeToGreet = "Evening";
+    }
+
+
     super.onInit();
   }
 
--- lib/app/modules/efb/pilot/views/main_view_pilot.dart
+++ lib/app/modules/efb/pilot/views/main_view_pilot.dart
@@ -2,23 +2,95 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../../presentation/theme.dart';
+import '../../../../../util/util.dart';
 import '../controllers/homepilot_controller.dart';
 
 class HomePilotView extends GetView<HomePilotController> {
   const HomePilotView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(HomePilotController());
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home FO View'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'Home FO View is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+        body: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Hi, ${controller.titleToGreet!}",
+                        style: tsOneTextTheme.headlineLarge,
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Good ${controller.timeToGreet}',
+                      style: tsOneTextTheme.labelMedium,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(
+                            Icons.calendar_month_outlined,
+                            color: TsOneColor.onSecondary,
+                            size: 32,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              Util.convertDateTimeDisplay(
+                                  DateTime.now().toString(), "EEEE"),
+                              style: tsOneTextTheme.labelSmall,
+                            ),
+                            Text(
+                              Util.convertDateTimeDisplay(
+                                  DateTime.now().toString(), "dd MMMM yyyy"),
+                              style: tsOneTextTheme.labelSmall,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    child: Text("Request Device",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 90), // Sesuaikan sesuai kebutuhan
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0), // Sesuaikan border radius sesuai kebutuhan
+                        ),
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.white, // Ganti dengan warna latar belakang yang diinginkan
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+        )
     );
   }
 }
