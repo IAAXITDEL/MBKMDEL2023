4ac77cb687efc2719e14ad0d83e07093df8319f1
Lile <70990797+Lilemanalu12S20017@users.noreply.github.com>
2023-10-04 16:35:41 +0700

    Add feature filter in list instructor and pilot on CPTS page

--- lib/app/modules/list_pilotcptscc/views/list_pilotcptscc_view.dart
+++ lib/app/modules/list_pilotcptscc/views/list_pilotcptscc_view.dart
@@ -1,126 +1,232 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../../../../presentation/shared_components/TitleText.dart';
 import '../../../../presentation/theme.dart';
+import '../../../../util/error_screen.dart';
+import '../../../../util/loading_screen.dart';
+import '../../../routes/app_pages.dart';
 import '../controllers/list_pilotcptscc_controller.dart';
 
 class ListPilotcptsccView extends GetView<ListPilotcptsccController> {
   const ListPilotcptsccView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final currentPageData = controller.getCurrentPageData();
+    Get.put(ListPilotcptsccController());
     return Scaffold(
       appBar: AppBar(
-        title: const RedTitleText(text: 'INSTRUCTORS',),
+        title: const RedTitleText(
+          text: 'PILOT / INSTRUCTOR',
+        ),
         backgroundColor: Colors.white,
         elevation: 0,
       ),
       body: Center(
         child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: controller.searchC,
-                  // onChanged: search,
-                  decoration: InputDecoration(
-                    hintText: "Search",
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(4.0)),
-                    focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-                    prefixIcon: const Icon(Icons.search),
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.clear), onPressed: () {
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Initially, fetch all users
+                      controller.setFilterType(FilterType.none);
+                      controller.getFilteredStream();
+                    },
+                    child: const Text('All'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Set the filter type to 'instructor' and fetch filtered users
+                      controller.setFilterType(FilterType.instructor);
+                      controller.getFilteredStream();
+                    },
+                    child: const Text('Instructors'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Set the filter type to 'pilot' and fetch filtered users
+                      controller.setFilterType(FilterType.pilot);
+                      controller.getFilteredStream();
+                    },
+                    child: const Text('Pilots'),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      // Set the filter type to 'none' and fetch filtered users
+                      controller.setFilterType(FilterType.none);
+                    },
+                    child: const Text('All'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Set the filter type to 'instructor' and fetch filtered users
+                      controller.setFilterType(FilterType.instructor);
+                    },
+                    child: const Text('Instructors'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Set the filter type to 'pilot' and fetch filtered users
+                      controller.setFilterType(FilterType.pilot);
+                    },
+                    child: const Text('Pilots'),
+                  ),
+
+                ],
+              ),
+
+              TextFormField(
+                controller: controller.searchC,
+                onChanged: (value) => controller.nameS.value = value,
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.clear),
+                    onPressed: () {
+                      controller.nameS.value = "";
+                      controller.getFilteredStream();
                       controller.searchC.clear();
                     },
-                    ),
                   ),
                 ),
-                const SizedBox(height: 20,),
-
-                Expanded(
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Obx(
+                () => Expanded(
                   child: SingleChildScrollView(
-                    child:ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: currentPageData.length,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            // Handle item tap
-                            // controller.toggleClick;
-                            // Get.toNamed(Routes.PILOT_CABIN_CREW_PROFILE);
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.all(5),
-                            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.8),
-                                  spreadRadius: 2,
-                                  blurRadius: 3,
-                                  offset: const Offset(0, 2),
-                                ),
-                              ],
-                            ),
-                            child: ListTile(
-                              leading: Image.asset("assets/images/user.png"),
-                              title: Text(currentPageData[index]["NAME"]),
-                              subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text('847598342385'),
-                                  Container(
-                                    padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+                    controller: controller.scrollController,
+                    child: StreamBuilder<List<Map<String, dynamic>>>(
+                      stream: controller.getFilteredStream(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return LoadingScreen(); // Placeholder while loading
+                        }
+                        if (snapshot.hasError) {
+                          return ErrorScreen();
+                        }
+
+                        var listAttendance = snapshot.data!;
+                        return Column(
+                          children: [
+                            ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: listAttendance.length,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                return InkWell(
+                                  onTap: () {
+                                    Get.toNamed(Routes.PILOTCREWDETAILCC,
+                                        arguments: {
+                                          "id": listAttendance[index]["ID NO"],
+                                        });
+                                  },
+                                  child: Container(
+                                    padding: const EdgeInsets.all(5),
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 5),
                                     decoration: BoxDecoration(
-                                      color: Colors.green,
                                       borderRadius: BorderRadius.circular(10),
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.3),
+                                          spreadRadius: 2,
+                                          blurRadius: 3,
+                                          offset: const Offset(0, 2),
+                                        ),
+                                      ],
                                     ),
-                                    child: const Text(
-                                      "Ready",
-                                      style: TextStyle(fontSize: 10, color: Colors.white),
+                                    child: ListTile(
+                                      leading: CircleAvatar(
+                                        radius: 20,
+                                        backgroundColor: Colors.black26,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(100),
+                                          child: listAttendance[index]
+                                                      ["PHOTOURL"] ==
+                                                  null
+                                              ? Image.asset(
+                                                  "assets/images/placeholder_person.png",
+                                                  fit: BoxFit.cover,
+                                                )
+                                              : Image.network(
+                                                  "${listAttendance[index]["PHOTOURL"]}",
+                                                  fit: BoxFit.cover),
+                                        ),
+                                      ),
+                                      title: Text(
+                                        listAttendance[index]["NAME"],
+                                        maxLines: 1,
+                                        style: tsOneTextTheme.labelMedium,
+                                      ),
+                                      subtitle: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            listAttendance[index]["ID NO"]
+                                                    .toString() ??
+                                                "",
+                                            style: tsOneTextTheme.labelSmall,
+                                          ),
+                                          Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 3, horizontal: 10),
+                                            decoration: BoxDecoration(
+                                              color:
+                                                  Colors.green.withOpacity(0.4),
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                            ),
+                                            child: const Text(
+                                              "Ready",
+                                              style: TextStyle(
+                                                  fontSize: 10,
+                                                  color: Colors.green),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      trailing: const Icon(Icons.navigate_next),
                                     ),
                                   ),
-                                ],
-                              ),
-                              trailing: const Icon(Icons.navigate_next),
+                                );
+                              },
                             ),
-                          ),
+                            controller.isLoading.value
+                                ? Center(
+                                    child: CircularProgressIndicator(),
+                                  )
+                                : SizedBox()
+                          ],
                         );
                       },
                     ),
                   ),
                 ),
-                // Wrap(
-                //   alignment: WrapAlignment.center,
-                //   children: List<Widget>.generate(
-                //     (controller.data.length / controller.itemsPerPage).ceil(),
-                //         (int index) {
-                //       final page = index + 1;
-                //       return ElevatedButton(
-                //
-                //         onPressed: () {
-                //          controller.goToPage(page);
-                //           print("$page");
-                //         },
-                //         child: Text('$page'),
-                //       );
-                //     },
-                //   ),
-                // ),
-
-              ],
-            )
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
-
-
