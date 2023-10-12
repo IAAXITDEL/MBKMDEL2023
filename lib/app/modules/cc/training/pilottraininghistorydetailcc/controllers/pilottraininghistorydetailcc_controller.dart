a9889483adf488db23b8c94836e503f59bc03448
Lile <70990797+Lilemanalu12S20017@users.noreply.github.com>
2023-10-12 14:05:27 +0700

    Display feedback in the history training page

--- lib/app/modules/cc/training/pilottraininghistorydetailcc/controllers/pilottraininghistorydetailcc_controller.dart
+++ lib/app/modules/cc/training/pilottraininghistorydetailcc/controllers/pilottraininghistorydetailcc_controller.dart
@@ -1,13 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:ts_one/presentation/view_model/attendance_detail_model.dart';
 
 import '../../../../../../presentation/view_model/attendance_model.dart';
 
 class PilottraininghistorydetailccController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   RxInt idTrainingType = 0.obs;
-  RxString idAttendance ="".obs;
-
+  RxString idAttendance = "".obs;
   RxString trainingName = "".obs;
 
   @override
@@ -28,41 +28,70 @@ class PilottraininghistorydetailccController extends GetxController {
 
   //Mendapatkan data kelas yang diikuti
   Future<List<Map<String, dynamic>>> getCombinedAttendance() async {
-    final attendanceQuery = await firestore.collection('attendance').where("id", isEqualTo: idAttendance.value).get();
+    final attendanceQuery = await firestore
+        .collection('attendance')
+        .where("id", isEqualTo: idAttendance.value)
+        .get();
+
+    final attendanceDetailQuery = await firestore
+        .collection('attendance-detail')
+        .where("idattendance", isEqualTo: idAttendance.value)
+        .get();
 
     List<Map<String, dynamic>> attendanceData = [];
 
     for (var doc in attendanceQuery.docs) {
-      final attendanceModel = AttendanceModel.fromJson(doc.data());
+      final attendanceDetailModel = AttendanceDetailModel.fromJson(doc.data());
 
-      // Ambil informasi pengguna hanya untuk instruktur yang terkait
-      final usersQuery = await firestore.collection('users').where("ID NO", isEqualTo: attendanceModel.instructor).get();
-      if (usersQuery.docs.isNotEmpty) {
-        final userData = usersQuery.docs[0].data();
+      for (var doc in attendanceQuery.docs) {
+        final attendanceModel = AttendanceModel.fromJson(doc.data());
 
-        // Ambil informasi yang diperlukan dari dokumen attendance
-        Map<String, dynamic> data = {
-          'subject': attendanceModel.subject,
-          'date': attendanceModel.date,
-          'name': userData['NAME'],
-          'department':  attendanceModel.department,
-          'trainingType': attendanceModel.trainingType,
-          'vanue': attendanceModel.vanue,
-          'room': attendanceModel.room,
-        };
+        // Ambil informasi pengguna hanya untuk trainer yang terkait
+        final trainersQuery = await firestore
+            .collection('users')
+            .where("ID NO", isEqualTo: attendanceModel.instructor)
+            .get();
 
-        // Tambahkan data ke list
-        attendanceData.add(data);
+        // Ambil informasi pengguna hanya untuk trainee yang terkait
+        final traineesQuery = await firestore
+            .collection('users')
+            .where("ID NO", isEqualTo: attendanceDetailModel.idtraining)
+            .get();
+
+        // Ambil informasi pengguna hanya untuk trainee yang terkait
+        final attendanceDetailsQuery = await firestore
+            .collection('attendance-detail')
+            .where("idattendance", isEqualTo: attendanceModel.id)
+            .get();
+        if (trainersQuery.docs.isNotEmpty) {
+          final trainerData = trainersQuery.docs[0].data();
+          final traineeData = traineesQuery.docs[0].data();
+          final attendanceDetailData = attendanceDetailsQuery.docs[0].data();
+
+          // Ambil informasi yang diperlukan dari dokumen attendance
+          Map<String, dynamic> data = {
+            'subject': attendanceModel.subject,
+            'date': attendanceModel.date,
+            'trainer-name': trainerData['NAME'],
+            'trainee-name' : traineeData['NAME'],
+            'department': attendanceModel.department,
+            'trainingType': attendanceModel.trainingType,
+            'vanue': attendanceModel.vanue,
+            'room': attendanceModel.room,
+            'feedback-from-trainer': attendanceDetailData['feedback'],
+            'feedback-from-trainee': attendanceDetailData['feedbackforinstructor'],
+          };
+
+          // Tambahkan data ke list
+          attendanceData.add(data);
+        }
+
+        trainingName.value = attendanceModel.subject!;
       }
-
-      trainingName.value = attendanceModel.subject!;
     }
 
     return attendanceData;
   }
-
-
-
 
   @override
   void onReady() {
@@ -73,6 +102,4 @@ class PilottraininghistorydetailccController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-
 }
